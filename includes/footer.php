a13b42063ac7ee5fdc31f27d658340414e7b2680
Jiří Čech <cech@iMac.local>
2020-06-03 18:05:20 +0200

    Configure markdon-it

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-      <footer>
+      <footer id="demo">
         <div class="row">
           <div class="col-lg-12">
             <p>&copy; 2017 | <a href="http://www.jiricech.com/" target="_blank"><b>Jiří Čech</b></a> All rights reserved</p>
--- public/js/markdown-it.js
+++ public/js/markdown-it.js
@@ -1,3 +1,4 @@
 var md = require('markdown-it')();
-var result = md.render('# markdown-it rulezz!');
+var result = md.render('## markdown-it rulezz!');
 console.log(result)
+document.getElementById("demo").innerHTML = result;
