57a7ddc6ca58a032ba5a9aae3c0cceb96d4b31d9
Jiří Čech <cech@iMac.local>
2020-06-03 16:38:59 +0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            <p>&copy; 2017 | <a href="http://www.jiricech.com/" target="_blank"><b>Jiří Čech</b></a> All rights reserved</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php include "includes/db.php"; ?>
+<?php include "admin/includes/functions.php"; ?>
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="This website was built for my learning purposes. Project is written in PHP and uses MySQL database. Bootstrap is used for sake of simplicity." />
+	<meta name="author" content="Jiří Čech" />
+	<meta name="keywords" content="cms, php, unsplash, photos, nature, buildings, architecture, space, bootstrap, mysql" />
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+  
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+
+	<link rel="stylesheet" type="text/css" href="css/blog-home.css" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+
+	<title>PHP CMS | Jiří Čech</title>
+</head>
+
+<body>
