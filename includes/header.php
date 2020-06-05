05b6fef50d5d1b5702d34a638bdbc0a8192f9c47
Jiří Čech <cech@iMac.local>
2020-06-05 12:14:14 +0200

    Add scss to webpack

--- includes/header.php
+++ includes/header.php
@@ -13,8 +13,6 @@
 	<meta name="keywords" content="cms, php, unsplash, photos, nature, buildings, architecture, space, bootstrap, mysql" />
   <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-
 	<link rel="stylesheet" type="text/css" href="css/blog-home.css" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 
--- src/application.js
+++ src/application.js
@@ -1,8 +1,10 @@
 import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 import Turbolinks from 'turbolinks';
+
 import "jquery";
 import "bootstrap";
+import "./stylesheets";
 
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
