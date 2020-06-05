608147fa6ad51d806d067e0ccb9a56f9c0a6e25a
Jiří Čech <cech@iMac.local>
2020-06-05 10:56:40 +0200

    Install turbolinks

--- src/application.js
+++ src/application.js
@@ -1,7 +1,9 @@
-// src/application.js
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Turbolinks from 'turbolinks';
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+Turbolinks.start();
