b5dbe9ce603994f1408f0924c831cb74e6988e90
Jiří Čech <cech@iMac.local>
2020-06-05 19:56:45 +0200

    Refactor admin navigation

---
+++ views/admin/application/_navigation.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="../admin">Guides Admin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="navbar-nav bg-dark mr-auto">
+      <li class='nav-item'><a class='nav-link' href="../../index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
+
+      <li class='nav-item dropdown'>
+        <a class='nav-link dropdown-toggle' href="#" id="postsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-fw fa-list" aria-hidden="true"></i> Posts <b class="caret"></b>
+        </a>
+
+        <div class="dropdown-menu bg-dark" aria-labelledby="postsDropdown">
+          <a class='nav-link' href="posts.php">View All Posts</a>
+          <a class='nav-link' href="posts.php?source=add_post">Add Post</a>
+        </div>
+      </li>
+
+      <li class='nav-item'><a class='nav-link' href="categories.php"><i class="fa fa-fw fa-folder"></i> Categories</a></li>
+      <li class='nav-item'><a class='nav-link' href="comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a></li>
+
+      <li class='nav-item dropdown'>
+        <a class='nav-link dropdown-toggle' href="#" id="usersDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-fw fa-user" aria-hidden="true"></i> Users <b class="caret"></b>
+        </a>
+
+        <div class="dropdown-menu bg-dark" aria-labelledby="postsDropdown">
+          <a class='nav-link' href="users.php">View All Users</a>
+          <a class='nav-link' href="users.php?source=add_user">Add User</a>
+        </div>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav bg-dark">
+      <li class='nav-item'><a class='nav-link' href="../index.php?page=1">Home</a></li>
+      <li class="nav-item dropdown">
+        <a class='nav-link dropdown-toggle' href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user"></i>
+          <?php
+          if(isset($_SESSION['username'])) {
+            echo $_SESSION['username'];
+          }
+          ?>
+          <b class="caret"></b>
+        </a>
+
+        <div class="dropdown-menu bg-dark dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class='nav-link' href="profile.php"><i class="fa fa-fw fa-cog"></i> Your Profile</a>
+          <a class='nav-link' href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ views/layouts/admin.php
@@ -0,0 +1,39 @@
+<?php ob_start(); ?>
+<?php session_start(); ?>
+<?php include "../includes/db.php"; ?>
+<?php include "../admin/includes/functions.php"; ?>
+<?php
+if(!isset($_SESSION['role'])) {
+  header("Location: ../index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="Administration layout for CMS." />
+  <meta name="author" content="Jiří Čech" />
+  <meta name="keywords" content="cms, php, bootstrap, mysql, admin" />
+  <link rel="icon" href="../../images/favicon.ico" type="image/x-icon" />
+
+  <script src="https://use.fontawesome.com/81ebd77a72.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+  <link href="css/sb-admin.css" rel="stylesheet" />
+  <link href="css/styles.css" rel="stylesheet" />
+
+  <script src="../build/public/bundle.js" async></script>
+
+  <title>Guides CMS Admin | Jiří Čech</title>
+</head>
+
+<body>
+<?php include "../views/admin/application/_navigation.php"; ?>
+<?php include($view); ?>
+<?php include "../views/application/_footer.php"; ?>
+</body>
+
+</html>
