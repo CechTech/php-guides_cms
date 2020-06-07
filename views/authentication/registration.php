613e72f43bba758fc2c03a06b0ca8383a850e71c
Jiří Čech <cech@iMac.local>
2020-06-07 21:02:49 +0200

    Add password confirmation

--- views/authentication/registration.php
+++ views/authentication/registration.php
@@ -3,15 +3,17 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
   $username = $_POST['username'];
   $email = $_POST['email'];
   $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
 
   $error = [
     'username'=> '',
     'email'=> '',
-    'password'=> ''
+    'password'=> '',
+    'confirm_password'=> ''
   ];
 
   if(strlen($username) < 4) {
-    $error['username'] = 'Username needs to be longer';
+    $error['username'] = 'Username needs to be at least 4 characters long';
   }
 
   if($username == '') {
@@ -19,7 +21,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
   }
 
   if(username_exists($username)) {
-    $error['username'] = 'Username already exists, pick another another';
+    $error['username'] = 'Username already exists, pick another';
   }
 
   if($email == '') {
@@ -32,6 +34,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
   if($password == '') {
     $error['password'] = 'Password cannot be empty';
+  }
+
+  if(strlen($password) < 6) {
+    $error['password'] = 'Password needs to be at least 6 characters long';
+  }
+
+  if($password != $confirm_password) {
+    $error['password'] = 'Passwords has to match';
   }
 
   foreach ($error as $key => $value) {
@@ -51,40 +61,41 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <div class="container">
   <section id="login">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-6 col-xs-offset-3">
-          <div class="form-wrap">
-            <h1>Register</h1>
-            <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-
-              <div class="form-group">
-                <label for="username" class="sr-only">username</label>
-                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username"
-                       autocomplete="on"
-                       value="<?php echo isset($username) ? $username : '' ?>">
-                <p><?php echo isset($error['username']) ? $error['username'] : '' ?></p>
-              </div>
-
-              <div class="form-group">
-                <label for="email" class="sr-only">Email</label>
-                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" autocomplete="on" value="<?php echo isset($email) ? $email : '' ?>" >
-
-                <p><?php echo isset($error['email']) ? $error['email'] : '' ?></p>
-              </div>
-
-              <div class="form-group">
-                <label for="password" class="sr-only">Password</label>
-                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
-
-                <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
-              </div>
-
-              <input type="submit" name="resgister" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
-            </form>
-
+    <div class="row justify-content-md-center">
+      <div class="col-sm-6">
+        <h1>Register</h1>
+        <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+          <div class="form-group">
+            <label for="username" class="sr-only">username</label>
+            <input type="text" name="username" id="username" class="form-control" placeholder="Username"
+                   autocomplete="on"
+                   value="<?php echo isset($username) ? $username : '' ?>">
+            <p><?php echo isset($error['username']) ? $error['username'] : '' ?></p>
           </div>
-        </div>
+
+          <div class="form-group">
+            <label for="email" class="sr-only">Email</label>
+            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" autocomplete="on" value="<?php echo isset($email) ? $email : '' ?>" >
+
+            <p><?php echo isset($error['email']) ? $error['email'] : '' ?></p>
+          </div>
+
+          <div class="form-group">
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+
+            <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
+          </div>
+
+          <div class="form-group">
+            <label for="confirm_password" class="sr-only">Confirm Password</label>
+            <input type="password" name="confirm_password" id="confirm_password" class="form-control" placeholder="Confirm Password">
+
+            <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
+          </div>
+
+          <input type="submit" name="resgister" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+        </form>
       </div>
     </div>
   </section>
